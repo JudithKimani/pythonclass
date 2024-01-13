41eebc988baf895e412202866f759d4964cb59d4
Judith Kim <nyakiokimani@gmail.com>
2024-01-13 12:32:20 +0300

    made changes

--- intro.py
+++ intro.py
@@ -7,3 +7,16 @@ sum(10, 40)
 print("I just updated this from github")
 
 print("I made it!")
+
+n = 10
+while n>0:
+    print(n, end=' ')
+    n = n - 1
+print('Done!')
+
+while True:
+    line = input('> ')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
